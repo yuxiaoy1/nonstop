53cb6f5342de0c86fb96bfaa357f471f93184e4e
yuxiaoy <1032897296@qq.com>
2020-11-24 15:10:32 +0800

    initial release

---
+++ 0.1.0/content-script.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict'
+    // this is for zhihu, weibo
+    let link = document.querySelector('.link')
+    if (link) {
+        window.location.href = link.textContent
+        console.log(link.textContent)
+        return
+    }
+    // this is for jianshu 
+    function getParams(field) {
+        let href = window.location.href;
+        let urlObject = new URL(href);
+        let res = urlObject.searchParams.get(field);
+        return res;
+    }
+    let url = getParams('url');
+    console.log(url)
+    window.location.href = url
+})();
